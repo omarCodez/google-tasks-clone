2c0e3a1310732f214bee98dd25a09435411c8d4b
Ridwan Adefemi Odubona <omaralli8088@gmail.com>
2023-04-06 16:14:59 +0100

    splash screen

---
+++ src/screens/SplashScreen.tsx
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from 'react';
+import {SafeAreaView, TouchableOpacity, Text, View, Image} from 'react-native';
+
+const landingImage = require('../assets/images/toks.png');
+
+const SplashScreen = () => {
+  return (
+    <SafeAreaView className="flex-1 p-5">
+      {/* top */}
+      <View className="w-[100%] h-[55%]">
+        <Image source={landingImage} alt="" className="w-full h-full" />
+      </View>
+      {/* bottom */}
+      <View className="h-[45%] items-center space-y-5 my-5">
+        <Text className="text-2xl">Welcome to Toks</Text>
+        <Text className="text-base text-center">
+          Keep track of important things you need to get done in one place.
+        </Text>
+        <TouchableOpacity
+          className="my-3 bg-blue-600 rounded-3xl"
+          activeOpacity={0.5}>
+          <Text className="text-lg  font-normal  text-white px-8 py-2.5 ">
+            Get Started
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default SplashScreen;
