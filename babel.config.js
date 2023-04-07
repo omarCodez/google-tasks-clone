e873ac2551f750b406be0733711e9265820af7e3
Ridwan Adefemi Odubona <omaralli8088@gmail.com>
2023-04-07 08:13:56 +0100

    added support for alias imports

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,22 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['nativewind/babel'],
+  plugins: [
+    'nativewind/babel',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@/assets': 'src/assets',
+          '@/components': 'src/components',
+          '@/constants': 'src/constants',
+          '@/screens': 'src/screens',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
+
+// react-native-reanimated has to be listed last.
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,8 +1,11 @@
+/* eslint-disable react/no-unstable-nested-components */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import React, {useState} from 'react';
 import {SafeAreaView, TouchableOpacity, Text, View, Image} from 'react-native';
 import {StarredTabScreen, TasksTabScreen} from './tabs';
+import IonIcon from 'react-native-vector-icons/Ionicons';
+import EvilIcon from 'react-native-vector-icons/EvilIcons';
 
 const HomeTab = createMaterialTopTabNavigator();
 
@@ -20,8 +23,30 @@ const HomeScreen = () => {
 
       {/* tab bottom */}
       <HomeTab.Navigator>
-        <HomeTab.Screen name="StarredTabScreen" component={StarredTabScreen} />
-        <HomeTab.Screen name="TasksTabScreen" component={TasksTabScreen} />
+        <HomeTab.Screen
+          name="StarredTabScreen"
+          component={StarredTabScreen}
+          options={{
+            tabBarItemStyle: {width: '20%'},
+            tabBarShowIcon: true,
+            tabBarShowLabel: false,
+            tabBarIcon: ({focused, color}) => {
+              return (
+                <View>
+                  <EvilIcon name="star" size={30} />
+                </View>
+              );
+            },
+          }}
+        />
+        <HomeTab.Screen
+          name="TasksTabScreen"
+          component={TasksTabScreen}
+          options={{
+            tabBarShowLabel: false,
+            tabBarItemStyle: {width: '80%'},
+          }}
+        />
       </HomeTab.Navigator>
     </SafeAreaView>
   );
